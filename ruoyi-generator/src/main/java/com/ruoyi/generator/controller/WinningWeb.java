8c47325f82d58873272615a4d584cc83c10c656a
LL <2447675152@qq.com>
2023-10-13 21:54:42 +0800

    修改模板文件

---
+++ ruoyi-generator/src/main/java/com/ruoyi/generator/controller/WinningWeb.java
@@ -0,0 +1,19 @@
+package com.ruoyi.generator.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+@RestController
+@RequestMapping("/winningWeb")
+@Slf4j
+public class WinningWeb {
+
+
+    public void main() {
+
+    }
+
+}
---
+++ ruoyi-generator/src/main/java/com/ruoyi/generator/pojo/ZyWinning.java
@@ -0,0 +1,16 @@
+package com.ruoyi.generator.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ZyWinning {
+
+    private String winningId;
+    private String userId;
+    private String lotteryId;
+    private String createTime;
+}
